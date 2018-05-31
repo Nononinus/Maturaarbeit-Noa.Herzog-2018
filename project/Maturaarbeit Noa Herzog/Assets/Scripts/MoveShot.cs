ac96bf47a33227373c2243fd9d12b8aba80c47d7
Noa <noa@stojanovic.ch>
2018-06-01 01:29:05 +0200

    Little Progress. Things done so far: -Createt a Player -Created a Controller for the Player -Let the player be able to shoot.
    Signed-off-by: Noa <noa@stojanovic.ch>

---
+++ project/Maturaarbeit Noa Herzog/Assets/Scripts/MoveShot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShot : MonoBehaviour {
+    //Movement
+    int xDelta, yDelta;
+    public Player p;
+    public Direction direction;
+
+	public void setShot(Player p, Direction direction)
+    {
+        this.p = p;
+        this.direction = direction;
+    }
+	void Start () {
+        Vector3 pos = p.transform.position;
+        transform.SetPositionAndRotation(pos, transform.rotation);
+        switch (direction)
+        {
+            case Direction.North:
+                yDelta = 1;
+                xDelta = 0;
+                break;
+            case Direction.South:
+                yDelta = -1;
+                xDelta = 0;
+                break;
+            case Direction.East:
+                yDelta = 0;
+                xDelta = 1;
+                break;
+            case Direction.West:
+                yDelta = 0;
+                xDelta = -1;
+                break;
+
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = transform.position.x + xDelta;
+        float y = transform.position.y + yDelta;
+        Vector3 pos = new Vector3(x, y, 0); 
+        transform.SetPositionAndRotation(pos, transform.rotation);
+    }
+}
--- project/Maturaarbeit Noa Herzog/Assets/Scripts/Player.cs
+++ project/Maturaarbeit Noa Herzog/Assets/Scripts/Player.cs
@@ -3,39 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Player : MonoBehaviour {
-    int x, y;
-    public bool u, d, r, l;
+    //Coordinates
+    int x, y, xdelta, ydelta;
     Vector3 pos;
-    Button up, down, left, right;
+    //Direction
+    
+    Direction headDir;
+    public GameObject shot;
+
 	// Use this for initialization
 	void Start () {
+        //Spawnpoint and heading
         y = 0;
         x = 0;
+        headDir = Direction.North;
+        
    
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        if (u)
+    //Set Direction
+    public void SetDir(int i)
+    {
+        switch (i)
         {
-
-        }else if (d)
-        {
-
-        }else if (r)
-        {
-            x = x + 1;
-            pos = new Vector3(x, y, 0);
-            transform.SetPositionAndRotation(pos, transform.rotation);
-        }else if (l)
-        {
+            case 1:
+                headDir = Direction.East;
+                break;
+            case 2:
+                headDir = Direction.South;
+                break;
+            case 3:
+                headDir = Direction.North;
+                break;
+            case 4:
+                headDir = Direction.West;
+                break;
 
         }
+    }
 
+    //Movement
+    public void SetXSpeed(int speed)
+    {
+        xdelta = speed;
+
+    }
+    public void SetYSpeed(int speed)
+    {
+        ydelta = speed;
+
+    }
+    //Shooting
+    public void Shoot()
+    {
+        shot.GetComponent<MoveShot>().setShot(this, headDir);
+        Instantiate(shot);
+        
+        
+    }
+
+    
+        
+    
+    // Update is called once per frame
+    void Update () {
+        //Movement
+        x = x + xdelta;
+        y = y + ydelta;
         pos = new Vector3(x, y, 0);
-        this.transform.SetPositionAndRotation(pos, this.transform.rotation);
+        transform.SetPositionAndRotation(pos, transform.rotation);
      
 	}
  
